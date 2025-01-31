59edf1fa2259a88fde77c3aae5acd80a7b3e0e96
Akshat Jain <akjn11@gmail.com>
2025-01-31 17:35:44 +0530

    debug 12

--- .github/scripts/create-jacoco-coverage-report.sh
+++ .github/scripts/create-jacoco-coverage-report.sh
@@ -4,48 +4,36 @@ set -e
 set -x
 
 echo "GITHUB_BASE_REF: ${GITHUB_BASE_REF}"
-
-echo "Printing git status:"
-git status
-
-echo "Printing git branch:"
-git branch
-
-echo "Printing git remote -v:"
-git remote -v
-
-echo "Printing all jacoco.exec files"
-find . -name '*jacoco*.exec'
+#
+#echo "Printing git status:"
+#git status
+#
+#echo "Printing git branch:"
+#git branch
+#
+#echo "Printing git remote -v:"
+#git remote -v
+#
+#echo "Printing all jacoco.exec files"
+#find . -name '*jacoco*.exec'
 
 echo "Setting up git remote"
 git remote set-branches --add origin ${GITHUB_BASE_REF}
 git fetch
 
-echo "Printing git branch:"
-git branch
-
-echo "Printing git branch -a:"
-git branch -a
+#echo "Printing git branch:"
+#git branch
+#
+#echo "Printing git branch -a:"
+#git branch -a
 
 # Compile the project. jacoco:report needs class files along with jacoco.exec files to generate the report.
-#mvn -B clean install -DskipTests -P skip-static-checks -Dweb.console.skip=true -Dmaven.javadoc.skip=true
-#
-## If there are multiple jacoco.exec files present in any module, merge them into a single jacoco.exec file for that module.
-#mvn jacoco:merge
-#
-#mvn jacoco:report
-#
-#echo "GITHUB_BASE_REF: ${GITHUB_BASE_REF}"
+mvn -B clean install -DskipTests -P skip-static-checks -Dweb.console.skip=true -Dmaven.javadoc.skip=true
 
-#changed_files_x="$(git diff --name-only origin/akshat/new-workflow...HEAD | grep "\.java$" || [[ $? == 1 ]])"
-#echo "changed_files_x: $changed_files_x"
-#
+# If there are multiple jacoco.exec files present in any module, merge them into a single jacoco.exec file for that module.
+mvn jacoco:merge
 
-echo "Printing git log --oneline -20 remotes/origin/akshat/new-workflow"
-git log --oneline -20 remotes/origin/akshat/new-workflow
-
-echo "Printing git log --oneline -20 origin/akshat/new-workflow"
-git log --oneline -20 origin/akshat/new-workflow
+mvn jacoco:report
 
 changed_files="$(git diff --name-only remotes/origin/${GITHUB_BASE_REF}...HEAD | grep "\.java$" || [[ $? == 1 ]])"
 
@@ -54,22 +42,22 @@ for f in ${changed_files}
 do
   echo $f
 done
-#
-#npm install @connectis/diff-test-coverage@1.5.3
-#
-#if [ -n "${changed_files}" ]
-#then
-#  git diff origin/${GITHUB_BASE_REF}...HEAD -- ${changed_files} |
-#  node_modules/.bin/diff-test-coverage \
-#  --coverage "**/target/site/jacoco/jacoco.xml" \
-#  --type jacoco \
-#  --line-coverage 50 \
-#  --branch-coverage 50 \
-#  --function-coverage 0 \
-#  --log-template "coverage-lines-complete" \
-#  --log-template "coverage-files-complete" \
-#  --log-template "totals-complete" \
-#  --log-template "errors" \
-#  -- ||
-#  { printf "\n\n****FAILED****\nDiff code coverage check failed. To view coverage report, run 'mvn clean test jacoco:report' and open 'target/site/jacoco/index.html'\nFor more details on how to run code coverage locally, follow instructions here - https://github.com/apache/druid/blob/master/dev/code-review/code-coverage.md#running-code-coverage-locally\n\n" && echo "coverage_failure=true" >> "$GITHUB_ENV" && false; }
-#fi
+
+npm install @connectis/diff-test-coverage@1.5.3
+
+if [ -n "${changed_files}" ]
+then
+  git diff origin/${GITHUB_BASE_REF}...HEAD -- ${changed_files} |
+  node_modules/.bin/diff-test-coverage \
+  --coverage "**/target/site/jacoco/jacoco.xml" \
+  --type jacoco \
+  --line-coverage 50 \
+  --branch-coverage 50 \
+  --function-coverage 0 \
+  --log-template "coverage-lines-complete" \
+  --log-template "coverage-files-complete" \
+  --log-template "totals-complete" \
+  --log-template "errors" \
+  -- ||
+  { printf "\n\n****FAILED****\nDiff code coverage check failed. To view coverage report, run 'mvn clean test jacoco:report' and open 'target/site/jacoco/index.html'\nFor more details on how to run code coverage locally, follow instructions here - https://github.com/apache/druid/blob/master/dev/code-review/code-coverage.md#running-code-coverage-locally\n\n" && echo "coverage_failure=true" >> "$GITHUB_ENV" && false; }
+fi
