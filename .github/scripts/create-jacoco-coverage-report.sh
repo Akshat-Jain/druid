4b4c6cfd051c587b5ba5c2c9eb41a33be54f51bb
Akshat Jain <akjn11@gmail.com>
2025-01-31 16:45:08 +0530

    Try using diff-test-coverage: Attempt 2

--- .github/scripts/create-jacoco-coverage-report.sh
+++ .github/scripts/create-jacoco-coverage-report.sh
@@ -3,6 +3,12 @@
 set -e
 set -x
 
+echo "Printing all jacoco.exec files"
+find . -name '*jacoco*.exec'
+
+echo "Setting up git remote"
+git remote set-branches --add origin ${GITHUB_BASE_REF} && git fetch
+
 # Compile the project. jacoco:report needs class files along with jacoco.exec files to generate the report.
 mvn -B clean install -DskipTests -P skip-static-checks -Dweb.console.skip=true -Dmaven.javadoc.skip=true
 
