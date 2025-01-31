fc1392455832abdc30ebe7466473b04c6ba51950
Akshat Jain <akjn11@gmail.com>
2025-01-31 17:23:01 +0530

    debug 8

--- .github/scripts/create-jacoco-coverage-report.sh
+++ .github/scripts/create-jacoco-coverage-report.sh
@@ -24,6 +24,9 @@ git fetch
 echo "Printing git branch:"
 git branch
 
+echo "Printing git branch -a:"
+git branch -a
+
 # Compile the project. jacoco:report needs class files along with jacoco.exec files to generate the report.
 #mvn -B clean install -DskipTests -P skip-static-checks -Dweb.console.skip=true -Dmaven.javadoc.skip=true
 #
@@ -34,9 +37,9 @@ git branch
 #
 #echo "GITHUB_BASE_REF: ${GITHUB_BASE_REF}"
 
-changed_files_x="$(git diff --name-only origin/akshat/new-workflow...HEAD | grep "\.java$" || [[ $? == 1 ]])"
-echo "changed_files_x: $changed_files_x"
-
+#changed_files_x="$(git diff --name-only origin/akshat/new-workflow...HEAD | grep "\.java$" || [[ $? == 1 ]])"
+#echo "changed_files_x: $changed_files_x"
+#
 changed_files="$(git diff --name-only origin/${GITHUB_BASE_REF}...HEAD | grep "\.java$" || [[ $? == 1 ]])"
 
 echo "Changed files:"
@@ -44,22 +47,22 @@ for f in ${changed_files}
 do
   echo $f
 done
-
-npm install @connectis/diff-test-coverage@1.5.3
-
-if [ -n "${changed_files}" ]
-then
-  git diff origin/${GITHUB_BASE_REF}...HEAD -- ${changed_files} |
-  node_modules/.bin/diff-test-coverage \
-  --coverage "**/target/site/jacoco/jacoco.xml" \
-  --type jacoco \
-  --line-coverage 50 \
-  --branch-coverage 50 \
-  --function-coverage 0 \
-  --log-template "coverage-lines-complete" \
-  --log-template "coverage-files-complete" \
-  --log-template "totals-complete" \
-  --log-template "errors" \
-  -- ||
-  { printf "\n\n****FAILED****\nDiff code coverage check failed. To view coverage report, run 'mvn clean test jacoco:report' and open 'target/site/jacoco/index.html'\nFor more details on how to run code coverage locally, follow instructions here - https://github.com/apache/druid/blob/master/dev/code-review/code-coverage.md#running-code-coverage-locally\n\n" && echo "coverage_failure=true" >> "$GITHUB_ENV" && false; }
-fi
+#
+#npm install @connectis/diff-test-coverage@1.5.3
+#
+#if [ -n "${changed_files}" ]
+#then
+#  git diff origin/${GITHUB_BASE_REF}...HEAD -- ${changed_files} |
+#  node_modules/.bin/diff-test-coverage \
+#  --coverage "**/target/site/jacoco/jacoco.xml" \
+#  --type jacoco \
+#  --line-coverage 50 \
+#  --branch-coverage 50 \
+#  --function-coverage 0 \
+#  --log-template "coverage-lines-complete" \
+#  --log-template "coverage-files-complete" \
+#  --log-template "totals-complete" \
+#  --log-template "errors" \
+#  -- ||
+#  { printf "\n\n****FAILED****\nDiff code coverage check failed. To view coverage report, run 'mvn clean test jacoco:report' and open 'target/site/jacoco/index.html'\nFor more details on how to run code coverage locally, follow instructions here - https://github.com/apache/druid/blob/master/dev/code-review/code-coverage.md#running-code-coverage-locally\n\n" && echo "coverage_failure=true" >> "$GITHUB_ENV" && false; }
+#fi
