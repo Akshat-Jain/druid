7d19ceae9c289edb08b5c295529c5cbb6db8ad5b
Akshat Jain <akjn11@gmail.com>
2025-02-04 16:04:55 +0530

    Address some of the review comments

--- .github/scripts/run-unit-tests.sh
+++ .github/scripts/run-unit-tests.sh
@@ -3,5 +3,9 @@
 set -e
 set -x
 
-SAFE_DTEST="$(echo "$1" | sed 's/[*]//g' | sed 's/,/-/g')"
-.github/scripts/run-maven-command.sh test -pl '!integration-tests,!:druid-it-tools,!:druid-it-image,!:druid-it-cases' -Dtest="$1" -Dsurefire.failIfNoSpecifiedTests=false -P skip-static-checks -Dweb.console.skip=true -Dmaven.javadoc.skip=true -Dmaven.test.failure.ignore=true -Djacoco.destFile="target/jacoco-${SAFE_DTEST}.exec"
+JACOCO_REPORT_HASH="$(echo "$*" | md5sum | cut -d ' ' -f1)"
+OPTS+="-pl !integration-tests,!:druid-it-tools,!:druid-it-image,!:druid-it-cases"
+OPTS+=" -Dsurefire.failIfNoSpecifiedTests=false -P skip-static-checks -Dweb.console.skip=true -Dmaven.javadoc.skip=true"
+OPTS+=" -Djacoco.destFile='target/jacoco-${JACOCO_REPORT_HASH}.exec'"
+
+mvn -B $OPTS test "$@"
